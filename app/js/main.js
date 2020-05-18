5d47842df08f31924e031b6a0ee7fa0a85b9cbf6
web-coder-maksim <clankedmax@gmail.com>
2020-05-18 22:48:15 +0300

    end project

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,30 @@
 $(function(){
 
+	$('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
+		var target = $(this).attr('href'),
+			 bl_top = $(target).offset().top;
+		$('body, html').animate({scrollTop: bl_top}, 700)
+		return false;
+	});
 
+(function($) {
+	$(function() {
+	
+		$('.company__scroll').click(function() {
+			$('html, body').animate({scrollTop: 0},700);
+			return false;
+		})
+	
+	})
+	})(jQuery);
+
+	$('.menu__burger').click(function(event){
+		$('.menu__burger, .menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+
+	$('.still').click(function(event){
+		$('.skills__items').toggleClass('active');
+	});
 
 });
